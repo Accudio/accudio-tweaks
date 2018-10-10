c84f7a3f1b8ca6277bb0d6953e5d32aa40731cda
Alistair Shepherd <alistair@accudio.com>
2018-10-10 22:49:00 +0100

    Added plugin tag to be compatible with github-updater

--- accudio-tweaks.php
+++ accudio-tweaks.php
@@ -6,14 +6,15 @@
  * @package     Accudio_Tweaks
  *
  * @wordpress-plugin
- * Plugin Name:     Accudio Tweaks
- * Plugin URI:      https://accudio.com/development
- * Description:     Custom tweaks to Wordpress, Woocommerce and Wordpress security.
- * Version:         2.0.0
- * Author:          Alistair Shepherd — Accudio
- * Author URI:      https://accudio.com/about
- * License:         GPL-3.0+
- * License URI:     http://www.gnu.org/licenses/gpl-3.0.txt
+ * Plugin Name:       Accudio Tweaks
+ * Plugin URI:        https://accudio.com/development
+ * Description:       Custom tweaks to Wordpress, Woocommerce and Wordpress security.
+ * Version:           2.0.0
+ * Author:            Alistair Shepherd — Accudio
+ * Author URI:        https://accudio.com/about
+ * License:           GPL-3.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ * GitHub Plugin URI: https://github.com/accudio/accudio-tweaks
  */
 
 // If this file is called directly, abort.
